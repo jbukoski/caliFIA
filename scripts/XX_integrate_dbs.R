92349c3217eeca1c7953e3bd0314e486ed9d8bcd
jbukoski <jacob.bukoski@yale.edu>
2020-08-03 19:14:33 -0600

    updating files

--- scripts/XX_integrate_dbs.R
+++ scripts/XX_integrate_dbs.R
@@ -125,3 +125,51 @@ names(linkTable)  # Confidential link table
 names(annTables)  # Annual FIA data
 names(prdcTables)  # Periodic FIA data
 names(r5Tables)  # Region 5 FIA data
+
+#-------------------------------
+# Linking the region 5 data
+
+# Need to use the forest level code (FORE) and the plot level code (PLOT)
+# to link the tables. FORE and R5_PLT_ID are a bit hidden. Can subtract
+# away region 5 indicator (500) to get FORE code whereas plot code may be
+# a 5 or 6 digit long number. 
+
+r5_linkData <- linkTable$LINK_CONF %>%
+  filter(!is.na(R5)) %>%
+  mutate(FORE = NFS_ADFORCD - 500,
+         R5_PLT_ID = ifelse(NFS_PLT_NUM_PNWRS < 100000, 
+                            NFS_PLT_NUM_PNWRS - (FORE * 1000), 
+                            NFS_PLT_NUM_PNWRS - (FORE * 10000))) %>%
+  select(BURN_CLASS:NFS_PLT_NUM_PNWRS, FORE, R5_PLT_ID, 
+         PERIODIC_PLOT_NBR_PNWRS:OAK_DEATH_PLOT_YN)
+
+# Link on both FORE & PLOT
+
+r5_linkData %>%
+  select(STATECD, COUNTYCD, PLOT_FIADB, FORE, R5_PLT_ID) %>%
+  left_join(r5Tables$R5_VEGETATION_DATA_ALL, by = c("FORE", "R5_PLT_ID" = "PLOT"))
+
+
+r5Tables$`R5 LIST OF CONDITIONS AND PLOTS IN IDB` %>%
+  filter(FOREST_OR_BLM_DISTRICT == 511) %>%
+  pull(CNTY) %>%
+  unique()
+
+colnames(r5Tables$`R5 LIST OF CONDITIONS AND PLOTS IN IDB`)
+
+
+
+linkTable$LINK_CONF %>%
+  filter(PLOT_FIADB %in% r5Tables$R5_VEGETATION_DATA_ALL$PLOT)
+
+annTables[["PLOT"]] %>%
+  filter(PLOT_FIADB %in% r5Tables[["R5 LIST OF CONDITIONS AND PLOTS IN IDB"]]$PLOT_ID)
+
+# Can link to CA91 periodic inventories using PLOT_FIADB
+
+linkTable[["LINK_CONF"]] %>%
+  filter(PLOT_FIADB %in% prdcTables[["CA81_TREE"]]$PLOT_FIADB) %>%
+  View
+
+
+
